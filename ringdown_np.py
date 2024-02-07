8629864d360222837720d1d2c7295c6257d2e49e
Sreelakshmi Mundakkal Dasan <136783615+sreelakshmi-mundakkal@users.noreply.github.com>
2024-02-07 10:24:59 +0530

    Update ringdown_np.py

--- ringdown_np.py
+++ ringdown_np.py
@@ -1,34 +1,65 @@
-# Copyright (C) 2020  Ssohrab Borhanian
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
+### addition in gwbench for ringdown-only analysis
+import pyRing
 import numpy as np
-from pycbc.waveform import ringdown
+import pycbc
+import scipy
+from scipy.interpolate import interp1d
+from pycbc.types import TimeSeries
+from pyRing import waveform as wf
+import sys
+sys.path.append('/home/sreelakshmi.m/gwmat/src')
+import py_lgw
+lgw = py_lgw.lensed_wf_gen()
 
-wf_symbs_string = 'f f_lmn tau amp phi_lmn t_0 l m n inclination azimuthal harmonics final_spin tc phic'
+wf_symbs_string = 'f t0 Mf af A_lmn D_L iota phi tc phic'
 
-def hfpc(f, f_lmn, tau, amp, phi_lmn, t_0, l, m, n, inclination , azimuthal, harmonics, final_spin, tc, phic):
-    f_min   = f[0]
-    delta_f = f[1] - f[0]
-    f_max   = f[-1] + delta_f
 
-   
+def hfpc(f,t0, Mf, af, A_lmn, D_L, iota, phi, tc, phic):
+    #f_min   = f[0]
+    #delta_f = f[1] - f[0]
+    #f_max   = f[-1] + delta_f
+
+    t0 = t0 
+    Mf = Mf
+    af = af   
+    amps = A_lmn
+    r = D_L
+    iota = iota
+    phi  = phi
     
-    hfp, hfc = ringdown.fd_damped_sinusoid(f_0 = f_lmn, tau= tau, amp=amp, phi= phi_lmn, freqs= f, t_0= t_0, l=l, m=m, n=n, inclination=inclination, azimuthal=azimuthal, harmonics=harmonics, 
-                    final_spin = final_spin)
-   
-
+    Kerr = wf.KerrBH(t0,Mf,af,amps,r,iota,phi)
+    t= np.arange(0.0033, 0.05, 2e-4)
+    waveform = Kerr.waveform(t)
+    hp = waveform[3]
+    hc = waveform[4]
+    hp = TimeSeries(hp, delta_t=t[1]-t[0])
+    hc = TimeSeries(hc, delta_t=t[1]-t[0])
+    hp_ = lgw.wf_len_mod_start(hp, extra=0.2, **{'sample_rate':hp.sample_rate})
+    hc_ = lgw.wf_len_mod_start(hc, extra=0.2, **{'sample_rate':hp.sample_rate})
+    f_hp = hp_.to_frequencyseries(delta_f=hp_.delta_f)
+    f_hc = hp_.to_frequencyseries(delta_f=hc_.delta_f)
     
-    return hfp, hfc
+    wfs_res = {'hp':f_hp, 'hc':f_hc}
+    res = dict()
+
+    frequency_vector = f #Desired frequency vector
+
+    for key in wfs_res.keys():
+
+        temp_fd = wfs_res[key]
+        log_strain_array = np.log10(np.array(temp_fd, dtype=np.complex128))
+        log_abs = np.real(log_strain_array)
+        phase = np.array(pycbc.waveform.utils.phase_from_frequencyseries(temp_fd))
+        if_log_abs = interp1d(temp_fd.sample_frequencies[:], log_abs[:], kind='linear')
+        if_phase = interp1d(temp_fd.sample_frequencies[:], phase[:], kind='linear')
+        interpolated_abs = np.concatenate(([0], 10**(if_log_abs(frequency_vector[1:]))))
+        interpolated_phase = np.concatenate(([0], if_phase(frequency_vector[1:])))
+        interpolated_strain = interpolated_abs * np.exp(1j * interpolated_phase)
+        frequency_bounds = (frequency_vector >= frequency_vector[0]) * (frequency_vector<= frequency_vector[-1])
+        interpolated_strain *= frequency_bounds
+        assert len(interpolated_strain) == len(f)
+        res[key] = interpolated_strain
+        hfp = res['hp']
+        hfc = res['hc']
+
+    return res['hp'], res['hc']
